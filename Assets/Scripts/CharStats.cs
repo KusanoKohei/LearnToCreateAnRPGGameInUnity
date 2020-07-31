91c1ff28c81a5ad07b468c5e4856489d80e45166
kusanokohei <kusanokohei@yahoo.co.jp>
2020-08-01 03:31:47 +0900

    初めてのコミットです
    Udemy『Learn To Create An RPG GameInUnity』内のコース38(PlayerStat)が終わったところです

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+    public string charName;
+    public int playerLevel = 1;
+    public int currentEXP;
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP;
+    public int strength;
+    public int defence;
+    public int wpnPwr;
+    public int armrPwr;
+    public string equippedWpn;
+    public string equippedArmr;
+    public Sprite charImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UIFade.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIFade : MonoBehaviour
+{
+    public static UIFade instance;
+
+    public Image fadeScreen;
+    public float fadeSpeed;
+
+    public bool shouldFadeToBlack;
+    public bool shouldFadeFromBlack;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldFadeToBlack)
+        {
+            // deltaTimeをかけるのは、プレイヤーごとに異なる環境であることを考慮して（フェード時間に差が生まれるのは良くない).
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1.0f, fadeSpeed * Time.deltaTime));  
+        
+            if(fadeScreen.color.a == 1.0f)
+            {
+                shouldFadeToBlack = false;
+            }
+        }
+
+        if (shouldFadeFromBlack)
+        {
+            // deltaTimeをかけるのは、プレイヤーごとに異なる環境であることを考慮して（フェード時間に差が生まれるのは良くない).
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+        
+            if(fadeScreen.color.a == 0f)
+            {
+                shouldFadeFromBlack = false;
+            }
+        }
+    }
+
+    public void FadeToBlack()
+    {
+        shouldFadeToBlack = true;
+        shouldFadeFromBlack = false;
+    }
+
+    public void FadeFromBlack()
+    {
+        shouldFadeToBlack = false;
+        shouldFadeFromBlack = true;
+    }
+}
