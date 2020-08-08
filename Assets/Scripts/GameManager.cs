c3fa53fdd33acbfb31c615ebca628e754184937b
kusanokohei <kusanokohei@yahoo.co.jp>
2020-08-08 09:44:34 +0900

    68 showing items to buy & sell  までOK

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,13 +8,13 @@ public class GameManager : MonoBehaviour
 
     public CharStats[] playerStats;
 
-    public bool gameMenuOpen , dialogActive , fadingBetweenAreas;
+    public bool gameMenuOpen , dialogActive , fadingBetweenAreas, shopActive;
 
     public string[] itemsHeld;
     public int[] numberOfItems;
     public Item[] referenceItems;
 
-
+    public int currentGold;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +22,14 @@ public class GameManager : MonoBehaviour
         instance = this;
 
         DontDestroyOnLoad(gameObject);
-        
+
+        SortItems();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(gameMenuOpen || dialogActive || fadingBetweenAreas)
+        if(gameMenuOpen || dialogActive || fadingBetweenAreas || shopActive)
         {
             PlayerController.instance.canMove = false;
         }
--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -29,6 +29,8 @@ public class GameMenu : MonoBehaviour
     public GameObject itemCharChoiceMenu;
     public Text[] itemCharChoiceNames;
 
+    public Text goldText;
+
     public static GameMenu instance;
 
 
@@ -85,6 +87,8 @@ public class GameMenu : MonoBehaviour
                 charStatHolder[i].SetActive(false);
             }
         }
+
+        goldText.text = GameManager.instance.currentGold.ToString() + "g";
     }
 
     public void ToggleWindow(int windowNumber)
